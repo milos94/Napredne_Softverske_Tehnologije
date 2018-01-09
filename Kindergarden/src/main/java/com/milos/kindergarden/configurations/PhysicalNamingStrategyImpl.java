d9c5ab4bb3f520debd1a52b4d835847b4cf35ab1
milos94 <a.milos94@gmail.com>
2018-01-09 08:34:18 +0100

    Added transactions

--- Kindergarden/src/main/java/com/milos/kindergarden/configurations/PhysicalNamingStrategyImpl.java
+++ Kindergarden/src/main/java/com/milos/kindergarden/configurations/PhysicalNamingStrategyImpl.java
@@ -1,38 +1,35 @@
 package com.milos.kindergarden.configurations;
 
-import java.io.Serializable;
 import java.util.Locale;
 
 import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 
-public class PhysicalNamingStrategyImpl extends PhysicalNamingStrategyStandardImpl implements Serializable {
+public class PhysicalNamingStrategyImpl extends PhysicalNamingStrategyStandardImpl {
 
- public static final PhysicalNamingStrategyImpl INSTANCE = new PhysicalNamingStrategyImpl();
+	private static final long serialVersionUID = 2038707682263851286L;
 
- @Override
- public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment context) {
-     return new Identifier(addUnderscores(name.getText()), name.isQuoted());
- }
+	public static final PhysicalNamingStrategyImpl INSTANCE = new PhysicalNamingStrategyImpl();
 
- @Override
- public Identifier toPhysicalColumnName(Identifier name, JdbcEnvironment context) {
-     return new Identifier(addUnderscores(name.getText()), name.isQuoted());
- }
+	@Override
+	public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment context) {
+		return new Identifier(addUnderscores(name.getText()), name.isQuoted());
+	}
 
+	@Override
+	public Identifier toPhysicalColumnName(Identifier name, JdbcEnvironment context) {
+		return new Identifier(addUnderscores(name.getText()), name.isQuoted());
+	}
 
- protected static String addUnderscores(String name) {
-     final StringBuilder buf = new StringBuilder( name.replace('.', '_') );
-     for (int i=1; i<buf.length()-1; i++) {
-        if (
-             Character.isLowerCase( buf.charAt(i-1) ) &&
-             Character.isUpperCase( buf.charAt(i) ) &&
-             Character.isLowerCase( buf.charAt(i+1) )
-         ) {
-             buf.insert(i++, '_');
-         }
-     }
-     return buf.toString().toLowerCase(Locale.ROOT);
- }
+	protected static String addUnderscores(String name) {
+		final StringBuilder buf = new StringBuilder(name.replace('.', '_'));
+		for (int i = 1; i < buf.length() - 1; i++) {
+			if (Character.isLowerCase(buf.charAt(i - 1)) && Character.isUpperCase(buf.charAt(i))
+					&& Character.isLowerCase(buf.charAt(i + 1))) {
+				buf.insert(i++, '_');
+			}
+		}
+		return buf.toString().toLowerCase(Locale.ROOT);
+	}
 }
